8d9afd009de839ce71195364fc14923041315222
Davi Pessanha <dpessanha@PI-Davi.productsintl>
2019-02-01 16:17:49 -0200

    update_book form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -153,8 +153,39 @@ exports.book_show_route = function(req, res, next) {
 };
 
 // Show edit form for one specific book
-exports.book_edit_route = function(req, res) {
-  res.send('NOT IMPLEMENTED: Book EDIT ROUTE');
+exports.book_edit_route = function(req, res, next) {
+  // Get book, authors and genres for form.
+  async.parallel({
+    book: function(callback) {
+        Book.findById(req.params.id)
+        .populate('author')
+        .populate('genre')
+        .exec(callback);
+    },
+    authors: function(callback) {
+        Author.find(callback);
+    },
+    genres: function(callback) {
+        Genre.find(callback);
+    },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.book==null) { // No results.
+            var err = new Error('Book not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Success.
+        // Mark our selected genres as checked.
+        for (var all_g_iter = 0; all_g_iter < results.genres.length; all_g_iter++) {
+            for (var book_g_iter = 0; book_g_iter < results.book.genre.length; book_g_iter++) {
+                if (results.genres[all_g_iter]._id.toString()==results.book.genre[book_g_iter]._id.toString()) {
+                    results.genres[all_g_iter].checked='true';
+                }
+            }
+        }
+        res.render('book_form', { title: 'Update Book', authors:results.authors, genres:results.genres, book: results.book });
+    });
 };
 
 // Update a particular book, then redirect somewhere
